7daed94922eeb8eb98b82c0bf3305f62e486cb2e
Vangala Aruna <arunajava567@gmail.com>
2025-08-19 15:07:51 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello {
+public static void main(String args[]){
+      System.out.println("Welcome to Java");
+System.out.println("Name:"+args[0]);
+System.out.println("Addres:"+args[1]);
+
+  }
+}
